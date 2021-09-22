fe9d4156b9aad2815375ce4aa37b0b084021da2d
Downinahill <brian.hill.0023@gmail.com>
2021-09-22 12:57:04 -0400

    added all my routes and my controller

--- Art/server.js
+++ Art/server.js
@@ -1,8 +1,54 @@
+// imports
 require('dotenv').config()
 const express = require('express')
 
 const app = express()
 const methodOverride = require('method-override')
 const session = require('express-session')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT
+
+// connect to database
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('db connected')
+})
+
+const db = mongoose.connection
+
+db.on('connected', () => {
+    console.log('mongoose connect to', MONGODB_URI)
+})
+
+db.on('disconnected', () => {
+    console.log('mongoose disconnect to', MONGODB_URI)
+})
+
+db.on('error', (error) => {
+    console.log('mongoose error', error)
+})
+
+// midddleware
+
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+
+//controllers
+
+const productsController = require('./controllers/artController')
+app.use('/art', artController)
+
+
+
+
+//listen
+
+app.listen(PORT, () => {
+    console.log('listen on port:', PORT)
+})
