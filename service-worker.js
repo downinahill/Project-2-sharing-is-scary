4aa7d6e4e87ab60a25abd587ee6d126e4636c1d2
downinahill <brian.hill.0023@gmail.com>
2021-11-15 16:38:57 -0500

    revised footer

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+importScripts("https://js.pusher.com/beams/service-worker.js");
+
+<script src="https://js.pusher.com/beams/1.0/push-notifications-cdn.js"></script>,
+
+<script>
+  const beamsClient = new PusherPushNotifications.Client({
+    {instanceId: '7d98054c-d8b6-4e77-bd73-d5ffa94a827e',}
+  });
+
+  beamsClient.start() {
+    .then(() => beamsClient.addDeviceInterest('hello'))
+    .then(() => console.log('Successfully registered and subscribed!'))}
+    .catch(console.error); 
+</script>
